215411366ea2a54780c8675828a76814dec4a2f6
ys984y <114851808+ys984y@users.noreply.github.com>
2023-07-27 21:53:19 +0530

    Create dsl.groovy

---
+++ dsl.groovy
@@ -0,0 +1,30 @@
+def jenkinsfile = "Jenkinsfile"
+
+def HEADER = '''
+<h4 style="color:blue;">Test Pipeline</h4>
+'''.stripIndent()
+
+pipelineJob("DemoPipeline") {
+    description "${HEADER}"
+    concurrentBuild(allowConcurrentBuild = true)
+    parameters {
+
+        stringParam (
+            'ENV',
+            'sd,
+            'Please input env'
+        )
+  
+    }
+
+    
+
+
+    logRotator(daysToKeep = 50, numToKeep = -1, artifactDaysToKeep = -1, artifactNumToKeep = -1)
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace(jenkinsfile))
+        }
+    }
+}
